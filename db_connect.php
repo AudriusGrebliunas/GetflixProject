dc6d22c9ebb54579903b52fe609e928150eb31c0
Gwendoline Jacobs <gwendolinejacobs1@gmail.com>
2023-11-28 17:29:38 +0100

    db updated

---
+++ db_connect.php
@@ -0,0 +1,17 @@
+<?php
+function connect(){
+$hostname="localhost";
+$dbname="salty";
+$username="root";
+$password="root";
+
+$dsn = "mysql:host=$hostname;dbname=$dbname";
+
+try {
+return $db = new PDO($dsn, $username, $password);
+}
+catch (Exception $e){
+  echo $e->getMessage();
+}
+}
+?>
