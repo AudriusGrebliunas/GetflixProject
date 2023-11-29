529a044374d6cc9f6f786bf6238baec0aca610cb
Ahmed Bounaazouz <ahmed@MacBook-Pro-de-Ahmed-2.local>
2023-11-29 10:37:18 +0100

    add template

--- Salty/src/App.jsx
+++ Salty/src/App.jsx
@@ -1,50 +1,35 @@
-import React, { useState } from "react";
-import 'tailwindcss/tailwind.css'; 
-const RegisterPage = () => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-  
-    const handleRegister = (event) => {
-      event.preventDefault();
-      console.log("Register with:", email, password);
-    };
-  
-    return (
-      <div className="flex flex-col items-center justify-center min-h-screen bg-gray-800">
-        <div className="text-7xl font-bold text-white mb-8">SALTY</div>
-        <form onSubmit={handleRegister} className="w-full max-w-xs">
-          <input
-            type="email"
-            name="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            className="bg-gray-700 text-white rounded py-3 px-4 mb-3 w-full"
-            placeholder="Email"
-            required
-          />
-          <input
-            type="password"
-            name="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            className="bg-gray-700 text-white rounded py-3 px-4 mb-3 w-full"
-            placeholder="Password"
-            required
-          />
-          <button
-            type="submit"
-            className="bg-gray-600 text-white rounded py-3 px-4 w-full "
-          >
-            Register
-          </button>
-        </form>
-        <div className="text-white mt-4">
-          <a href="/forgot-password" className="hover:text-gray-300">
-            mot de passe oublié
-          </a>
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
         </div>
+        <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
       </div>
-    );
-  };
-  
-  export default RegisterPage;
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
