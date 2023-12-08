ed79c5a301110529d5ba8b621ed396cbf2dd48ca
AudriusGrebliunas <audrius.grebliunas@gmail.com>
2023-12-08 11:37:12 +0100

    update userProfiles

--- user/userProfiles.php
+++ user/userProfiles.php
@@ -15,7 +15,7 @@ function createResponse($status, $message, $data = [])
 
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
 
-    $queryUserData = $db->query("SELECT first_name, last_name, address, email, dob FROM users");
+    $queryUserData = $db->query("SELECT first_name, last_name, address, email, dob, question FROM users");
     try {
         $UserData=$queryUserData->fetchAll(PDO::FETCH_ASSOC);
         if ($UserData) {
