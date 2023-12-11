2bdea20378ae339c0d9601db17aec5f5d4027705
Ahmed Bounaazouz <ahmedbounaazouz@Ahmeds-MacBook-Pro.local>
2023-12-11 20:54:53 +0100

    add register

--- SaltyPlay/src/App.jsx
+++ SaltyPlay/src/App.jsx
@@ -1,65 +1,28 @@
-import React, { useState } from 'react';
-import './App.css';
-import 'tailwindcss/tailwind.css';
-import axios from 'axios';
+import { useState } from 'react'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import Connexion from './Compenant/Connexion'
+import Register from './Compenant/Register'
 
-const RegisterPage = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+export default function App() {
 
-  const handleRegister = async (event) => {
-    event.preventDefault();
-
-    try {
-      const response = await axios.post('http://localhost/back-end/user/login.php', {
-        email: email,
-        password: password,
-      });
-
-      
-      console.log(response.data);
-
-    } catch (error) {
-      console.error('Erreur lors de la connexion:', error);
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Connexion />
+    },
+    {
+      path: "/register",
+      element: <Register />
     }
-  };
-
+  ]);
+  
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-800">
-      <div className="text-7xl font-bold text-white mb-8">SALTY</div>
-      <form onSubmit={handleRegister} className="w-full max-w-xs">
-        <input
-          type="email"
-          name="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          className="bg-gray-700 text-white rounded py-3 px-4 mb-3 w-full"
-          placeholder="Email"
-          required
-        />
-        <input
-          type="password"
-          name="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          className="bg-gray-700 text-white rounded py-3 px-4 mb-3 w-full"
-          placeholder="Password"
-          required
-        />
-        <button
-          type="submit"
-          className="bg-gray-600 text-white rounded py-3 px-4 w-full"
-        >
-          Register
-        </button>
-      </form>
-      <div className="text-white mt-4">
-        <a href="/forgot-password" className="hover:text-gray-300">
-          mot de passe oublié
-        </a>
-      </div>
+    <div className="App">
+      <RouterProvider router={router} />
     </div>
-  );
-};
-
-export default RegisterPage;
+  )
+}
