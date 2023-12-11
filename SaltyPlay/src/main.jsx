daf9e1e301f65348787cd2b93ee9575824bb0ec6
Ahmed Bounaazouz <ahmedbounaazouz@Ahmeds-MacBook-Pro.local>
2023-12-11 20:56:33 +0100

    add register

--- SaltyPlay/src/main.jsx
+++ SaltyPlay/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css';
-import 'tailwindcss/tailwind.css'
+// import 'tailwindcss/tailwind.css'
 // import Navbar from './Compenant/navbar.jsx';
 // import CategoryPage from './categorie.jsx';
 // import Nouveaute from './nouveaute.jsx';
