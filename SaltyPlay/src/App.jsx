130d9585df63d1abe1dea110b152c26e464327b0
ahmedymh <ahmedbnz47@gmail.com>
2023-12-13 19:18:40 +0100

    Path

--- SaltyPlay/src/App.jsx
+++ SaltyPlay/src/App.jsx
@@ -1,28 +1,29 @@
-import { useState } from 'react'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { useState } from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
-import Connexion from './Compenant/Connexion'
-import Register from './Compenant/Register'
+import Connexion from "./Compenant/Connexion";
+import Register from "./Compenant/Register";
+import Wishlist from "./wishlist";
 
 export default function App() {
-
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <Connexion />
+      element: <Connexion />,
     },
     {
       path: "/register",
-      element: <Register />
-    }
+      element: <Register />,
+    },
+    {
+      path: "/wishlist",
+      element: <Wishlist />,
+    },
   ]);
-  
+
   return (
     <div className="App">
       <RouterProvider router={router} />
     </div>
-  )
+  );
 }
