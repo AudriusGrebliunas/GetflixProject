6395cadedc9ba06cf6ee7aa5dfa06ad987eda3aa
ahmedymh <ahmedbnz47@gmail.com>
2023-12-15 15:23:38 +0100

    Modal video

--- SaltyPlay/src/Compenant/Modal.jsx
+++ SaltyPlay/src/Compenant/Modal.jsx
@@ -10,41 +10,42 @@ const VideoPlayer = ({ youtubeUrl }) => {
   const videoId = extractVideoIdFromUrl(youtubeUrl);
 
   const opts = {
-    height: '390',
-    width: '640',
     playerVars: {
-      autoplay: 0,
+      autoplay: 1,
+      controls: 0,
+      modestbranding: 1,
+      loop: 1,
+      playlist: videoId,
     },
   };
 
   return (
-    <div>
-      {videoId ? (
-        <YouTube videoId={videoId} opts={opts} />
-      ) : (
-        <p>URL YouTube non valide.</p>
-      )}
+    <div className="flex flex-col object-cover w-[100%] h-[100%]">
+      {videoId && <YouTube videoId={videoId} opts={opts} className=" object-cover" />}
     </div>
   );
 };
 
 const Modal = ({ movie, onClose }) => {
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center">
-      <div className="absolute inset-0 bg-black opacity-50"></div>
-      <div className="bg-white p-8 rounded-lg z-10 h-[80%] w-[80%]">
+    <div className="fixed inset-0 flex flex-col items-center justify-center h-screen w-screen">
+      <div className="bg-gray-700 p-8  rounded-lg z-20 relative h-full w-full bottom-0">
         <div className="flex justify-end">
-          <button className="text-2xl text-gray-600" onClick={onClose}>&times;</button>
+          <button className="text-2xl text-white fixed absolute top-0" onClick={onClose}>&times;</button>
         </div>
-        <h2 className="text-3xl font-semibold mb-4">{movie.name}</h2>
-        <h2 className="text-3xl font-semibold mb-4">{movie.author}</h2>
-        <h2 className="text-3xl font-semibold mb-4">{movie.resume}</h2>
-        <h2 className="text-3xl font-semibold mb-4">{movie.year}</h2>
+        <div className='flex flex-col mb-0 text-white gap-40'>
+        <div className='flex justify-center items-center '>
         <VideoPlayer youtubeUrl={movie.link_yt} />
+        </div>
+        <div>
+          <h2 className="text-3xl font-semibold mb-4">{movie.name}</h2>
+          <h2 className="text-3xl font-semibold mb-4">{movie.author}</h2>
+          <h2 className="text-3xl font-semibold mb-4">{movie.resume}</h2>
+          </div>
+        </div>
       </div>
     </div>
   );
 };
 
 export default Modal;
-
